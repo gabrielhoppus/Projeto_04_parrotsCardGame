ed5c6c6c0ec17aaee4b3f2f0d3a11a272d4b2034
gabrielhoppus <luizgabriel.84@hotmail.com>
2022-10-20 11:45:27 -0300

    Layout  + Card Randomizer V3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,36 @@
 const div = document.querySelector(".card_container");
+let card_list = []
 
 function turnCard(parrot){
     parrot.classList.toggle("hidden")
 }
 
+function randomizeList(){
+    return Math.random() - 0.5;
+}
+
 function addCard(){
     let cards = 
         Number(prompt("Quantas cartas você quer jogar? (entre 4 e 14, número par)"));
-    
     let i = 0
-    
+    let image = 1
     if (cards >= 4 && cards <= 14 && cards % 2 == 0){
         while (i < cards){
-            div.innerHTML += 
-            `<div onclick="turnCard(this)" class="parrot_card">
-                <img class="parrot_back" src="./assets/back.png">
-                <img class="parrot_front hidden" src="./assets/bobrossparrot.gif">
-            </div>`
+            let card = 
+                    `<div onclick="turnCard(this)" class="parrot_card">
+                        <img class="parrot_back" src="./assets/back.png">
+                        <img class="parrot_front hidden" src="./assets/${Math.floor(image)}.gif">
+                    </div>`
+            card_list.push(card)
             i++
+            image += 0.5
         } 
     }else{
         return addCard()
     }
+    card_list.sort(randomizeList)
+    for (let i=0; i < card_list.length; i++){        
+        div.innerHTML += card_list[i]
+    }
+
 }
